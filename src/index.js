b3f5743feb5e2bf6d32505dcedfbcd0874a7dd4e
HIEU VU THANH <jezzs2k@gmail.com>
2020-08-08 08:31:46 +0700

    I am have setUp base basic when start feature auth

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const Start_Route = require('./Route/start.route');
+const Auth_Route = require('./Route/auth.route');
 
 const connectServer = (app) => {
   app.use('/', Start_Route);
+  app.use('/api', Auth_Route);
 
   const PORT = process.env.PORT || 8080;
 
