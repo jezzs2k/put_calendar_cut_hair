c1048f070d4307ddf61d24f054201f7d3a5d4e79
HIEU VU THANH <jezzs2k@gmail.com>
2020-08-08 17:24:52 +0700

    delete controller and route because not necessearly

--- src/Route/time.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const route = express.Router();
-
-const CheckToken = require('../middleware/checkToken');
-const { Set_Time_Controller } = require('../controller/time.controller');
-
-route.post('/times', CheckToken, Set_Time_Controller);
-
-module.exports = route;
