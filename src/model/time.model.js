58b09138556dac1dd4f8f35b3266b005e7f01c62
HIEU VU THANH <jezzs2k@gmail.com>
2020-08-08 17:22:43 +0700

    delete time

--- src/model/time.model.js
+++ src/model/time.model.js
@@ -1,3 +1,13 @@
 const Time = require('../schema/time.shema');
 
-module.exports = {};
+const Delete_Time = async ({ planId }) => {
+  try {
+    const time = await Time.findOneAndDelete({ plan: planId });
+
+    return { time };
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = { Delete_Time };
