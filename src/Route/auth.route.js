4caf8df25799be8857da8784ced607a02c55fb89
HIEU VU THANH <jezzs2k@gmail.com>
2020-08-08 09:27:44 +0700

    I already completed this feature

--- src/Route/auth.route.js
+++ src/Route/auth.route.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const route = express.Router();
 
-const {} = require('../controller/auth.controller');
+const { Register_Controller } = require('../controller/auth.controller');
+
+route.post('/auth/register', Register_Controller);
 
 module.exports = route;
--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -1,3 +1,14 @@
-const {} = require('../model/auth.model');
+const { Register } = require('../model/auth.model');
 
-module.exports = {};
+const Register_Controller = async (req, res) => {
+  try {
+    const result = await Register(req.body);
+
+    res.status(201).json(result);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: 'SERVER UNKNOWN' });
+  }
+};
+
+module.exports = { Register_Controller };
