122a38100c78ee5591d61c5b411c709b3f916839
HIEU VU THANH <jezzs2k@gmail.com>
2020-08-08 17:16:22 +0700

    fix more error

--- src/model/time.model.js
+++ src/model/time.model.js
@@ -1,8 +1,8 @@
 const Time = require('../schema/time.shema');
 
-const Set_Time = async ({ startTime, endTime }) => {
+const Set_Time = async ({ startTime, endTime, planId }) => {
   try {
-    const time = new Time({ startTime, endTime });
+    const time = new Time({ startTime, endTime, plan: planId });
 
     (async function () {
       await time.save();
