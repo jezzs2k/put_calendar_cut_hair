d08c7e979f3940c54f4ea6bcbf60dc803b6e1ccf
HIEU VU THANH <jezzs2k@gmail.com>
2020-08-08 10:57:04 +0700

    setup feature concorn User

---
+++ src/Route/user.route.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const route = express.Router();
+
+const {} = require('../controller/user.controller');
+
+module.exports = route;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const Start_Route = require('./Route/start.route');
 const Auth_Route = require('./Route/auth.route');
+const User_Route = require('./Route/user.route');
 
 const connectServer = (app) => {
   app.use('/', Start_Route);
   app.use('/api', Auth_Route);
+  app.use('/api', User_Route);
 
   const PORT = process.env.PORT || 8080;
 
