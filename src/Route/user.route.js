5dd7e0a8b7c633d860585fa1e181c3dd4cdddfad
HIEU VU THANH <jezzs2k@gmail.com>
2020-08-08 14:37:54 +0700

    Add checkToken into the route getUser

--- src/Route/user.route.js
+++ src/Route/user.route.js
@@ -1,9 +1,10 @@
 const express = require('express');
-
 const route = express.Router();
+
+const CheckToken = require('../middleware/checkToken');
 
 const { Get_User_Controller } = require('../controller/user.controller');
 
-route.get('/user', Get_User_Controller);
+route.get('/user', CheckToken, Get_User_Controller);
 
 module.exports = route;
