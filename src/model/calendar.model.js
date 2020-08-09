91e1f62bec8420bcc17cabf8f169fd58af18b9f5
HIEU VU THANH <jezzs2k@gmail.com>
2020-08-09 09:23:11 +0700

    add relationship with 'time'

--- src/model/calendar.model.js
+++ src/model/calendar.model.js
@@ -2,7 +2,7 @@ const Calendar = require('../schema/calendar.schema');
 
 const Get_Calendar = async () => {
   try {
-    const calendars = await Calendar.find().populate(['user']);
+    const calendars = await Calendar.find().populate(['user', 'time']);
 
     return { calendars: [...calendars] };
   } catch (error) {
